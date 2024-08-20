549a68fd9147e3335ad5ed3c6f99c607b2b9ed0c
nahiankhan7 <thenewkhan7@gmail.com>
2024-08-20 17:00:22 +0600

    coming soon

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,40 @@
+const notifyMeButton = document.getElementById("notifyMe");
 const moreInfoButton = document.getElementById("moreInfo");
+const closeButton = document.getElementById("closeButton");
+
 const homePage = document.getElementById("homePage");
 const aboutUs = document.getElementById("aboutUs");
-moreInfoButton?.addEventListener("click", () => {
-  if (homePage) {
-    homePage.style.width = "50%";
+
+const modal = document.getElementById("modal");
+const modalCloseButton = document.getElementById("modalClose");
+
+notifyMeButton?.addEventListener("click", () => {
+  if (modal) {
+    modal.style.display = "block";
   }
-  if (aboutUs) {
+});
+
+modalCloseButton?.addEventListener("click", () => {
+  if (modal) {
+    modal.style.display = "none";
+  }
+});
+
+moreInfoButton?.addEventListener("click", () => {
+  if (homePage && aboutUs && closeButton) {
+    homePage.style.width = "50%";
     aboutUs.style.left = "50%";
+
+    setTimeout(() => {
+      closeButton.style.display = "block";
+    }, 1000);
+  }
+});
+
+closeButton?.addEventListener("click", () => {
+  if (homePage && aboutUs && closeButton) {
+    homePage.style.width = "100%";
+    aboutUs.style.left = "100%";
+    closeButton.style.display = "none";
   }
 });
